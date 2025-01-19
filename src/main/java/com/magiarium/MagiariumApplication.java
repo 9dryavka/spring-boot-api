dccacd1cb4c9b8e03753773bd956303da09207b7
9dryavka <9dryavkadog@gmail.com>
2025-01-19 11:31:47 +0900

    初回登録

---
+++ src/main/java/com/magiarium/MagiariumApplication.java
@@ -0,0 +1,13 @@
+package com.magiarium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagiariumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagiariumApplication.class, args);
+	}
+
+}
