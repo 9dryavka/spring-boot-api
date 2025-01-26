fd8177f416d55958a7b9e5bdc80a47c8127c2935
9dryavka <9dryavkadog@gmail.com>
2025-01-26 19:30:28 +0900

    追加修正

---
+++ src/main/java/com/magiarium/domain/entity/ContentMaster.java
@@ -0,0 +1,65 @@
+package com.magiarium.domain.entity;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.magiarium.domain.data.ItemTypeEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
+@Table(name = "content_master", uniqueConstraints = {@UniqueConstraint(columnNames = {"item_type", "label"})})
+public class ContentMaster {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "item_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    ItemTypeEnum itemType;
+
+    @Column(name = "title", length = 45, nullable = false)
+    String title;
+
+    @Column(name = "description")
+    String description;
+
+    @Column(name = "content_json", nullable = false, columnDefinition = "JSON")
+    String contentJson;
+
+    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    Timestamp createdAt;
+
+    @Column(name = "updated_at", nullable = false, insertable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    Timestamp updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = new Timestamp(new Date().getTime());
+        updatedAt = new Timestamp(new Date().getTime());
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = new Timestamp(new Date().getTime());
+    }
+
+    @OneToMany(mappedBy = "content")
+    private List<ContentResource> contentResources = new ArrayList<>();
+
+}
